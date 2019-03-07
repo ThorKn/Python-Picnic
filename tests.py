07d85ecf5cbaca924473ed0d1d3f5e8bddf56fb0
Thorsten Knoll <thorsten_knoll@yahoo.de>
2019-03-07 12:17:54 +0100

    docs: last words written. correction still to be done.

--- tests.py
+++ tests.py
@@ -9,33 +9,49 @@ picnic = Picnic()
 priv_key = bytes([0xA5, 0x2A, 0x6C, 0x86, 0xC2, 0x9A, 0x19, 0x3B, 0x42, 0xE9, 0x97, 0xAC, 0xAC, 0xB2, 0x66, 0x03])  
 p        = bytes([0x95, 0xB3, 0xB0, 0x21, 0x8B, 0x6D, 0xFE, 0xEE, 0x04, 0x9D, 0xF0, 0x22, 0x6E, 0x5B, 0xB5, 0xEA])
 
+# -------------------------------------
 # Generate keys from testvectors above
-# picnic.generate_keys(p = p, priv_key = priv_key)
+# -------------------------------------
+picnic.generate_keys(p = p, priv_key = priv_key)
 
+# -------------------------------------
 # Generate random keys
-picnic.generate_keys()
+# -------------------------------------
+# picnic.generate_keys()
 
+# --------------------------------
 # Create message and then sign it:
 # --------------------------------
 message = bytearray([0x01] * 500)
 picnic.sign(message)
 
-# Print, serialize and save the signature:
 # ----------------------------------------
-# picnic.print_signature()
+# Serialize and save the signature:
+# ----------------------------------------
 picnic.serialize_signature()
-picnic.write_ser_sig_to_file('signature_random_keys.txt')
+picnic.write_ser_sig_to_file('signature.txt')
 
-# Read an write the ser sig again:
-# --------------------------------
+# ---------------------------------
+# Read an write the ser sig again.
+# The two files then could be
+# compared with "diff" to be equal.
+# ---------------------------------
 # picnic.read_ser_sig_from_file('signature.txt')
 # picnic.deserialize_signature()
 # picnic.serialize_signature()
 # picnic.write_ser_sig_to_file('signature_2.txt')
 
-# picnic.verify(message)
+# -----------------------------------------
+# Read, deserialize and verify a signature
+# -----------------------------------------
+picnic.read_ser_sig_from_file('signature.txt')
+picnic.deserialize_signature()
+picnic.verify(message)
 
-# Print the serialized signature:
-# -------------------------------
+# --------------------------------
+# Print the signatures in console:
+# --------------------------------
+# picnic.print_signature()
 # picnic.print_signature_ser()
+
 
